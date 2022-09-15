9fd9d92ad034b8bd1dbdd00ab95633a7c9fc8fc5
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-09-15 22:40:39 +0600

    Blog HTML File Added

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  const intTimeTaken = parseInt(timeTaken);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -85,12 +86,12 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${intTimeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
-  addHistory(questionText, timeTaken, errorCount);
+  addHistory(questionText, intTimeTaken, errorCount);
 
   // restart everything
   startTime = null;
